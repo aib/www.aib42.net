d4d77272ec726451029bf462221a4d6b69b64906
aib <aibok42@gmail.com>
2017-03-29 15:42:22 +0300

    Add article support: /articles and /article/<id> routes

--- server/routes.py
+++ server/routes.py
@@ -1,14 +1,42 @@
 import flask
 
+import articles
+
+ARTICLE_DIR = '../articles'
 STATIC_DIR = '../html'
 TEMPLATE_DIR = '../templates'
 
 def process(app):
 	app.template_folder = TEMPLATE_DIR
 
+	articles_ = articles.Articles()
+	articles_.load_from_dir(ARTICLE_DIR)
+
 	@app.route('/')
 	def root_r():
 		return flask.render_template('index.html')
+
+	@app.route('/articles')
+	def articles_r():
+		def article_view(a):
+			return {
+				'id': a.id,
+				'title': a.title,
+				'date': a.date,
+				'href': flask.url_for('article_r', article_id=a.id)
+			}
+		return flask.render_template('articles.html', articles=map(article_view, articles_.list_by_date()))
+
+	@app.route('/article/<article_id>')
+	def article_r(article_id):
+		article = articles_.get(article_id)
+		view = {
+			'title': article.title,
+			'author': article.authors,
+			'date': article.date,
+			'content': article.content
+		}
+		return flask.render_template('article.html', **view)
 
 	@app.route('/<path:path>')
 	def get_path_r(path):
